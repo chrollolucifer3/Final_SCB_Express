5843462871c73ee8cb11cdde336f078893eb5f68
CuongNguyen <chrollolucifer97@gmail.com>
2024-02-03 20:31:13 +0700

    last commit

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Board_DB');
+        await mongoose.connect('mongodb+srv://chrollolucifer208:Cuong0208@cluster0.hbgdphu.mongodb.net/?retryWrites=true&w=majority');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
